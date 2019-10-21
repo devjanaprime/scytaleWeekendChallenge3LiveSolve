8dc5e6cabe37e4dc45971b726cc864f1cb9c741f
Dev Jana <devjana@Devs-MacBook-Pro-2.local>
2019-10-21 09:26:00 -0500

    project base ready

---
+++ server/server.js
@@ -0,0 +1,12 @@
+// requires
+const express = require( 'express' );
+const app = express();
+const bodyParser = require( 'body-parser' );
+// uses
+app.use( express.static( 'server/public' ) );
+// globals
+const port=5000;
+// spin up server
+app.listen( port, ()=>{
+    console.log( 'server is up on:', port );
+}) // end server up
